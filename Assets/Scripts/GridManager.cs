8f4ebf9a1264d11720f5f1071ce822203807b754
PKJ <danhana123@naver.com>
2023-03-14 22:22:02 +0900

    TilemapEditor 0.01V
    Add Tilemap

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private Tile _tilePrefab;
+
+    [SerializeField] private Transform _cam;
+
+    private Dictionary<Vector2, Tile> _tiles;
+
+    private void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        _tiles = new Dictionary<Vector2, Tile>();
+        for(int x = 0; x < _width; x++)
+        {
+            for(int y = 0; y < _height; y++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
+
+                _tiles[new Vector2(x, y)] = spawnedTile;
+            }
+        }
+
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+    }
+
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if(_tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color _baseColor, _offsetColor;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject _highlight;
+
+    public void Init(bool isOffset)
+    {
+        _renderer.color = isOffset ? _offsetColor : _baseColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        _highlight.SetActive(false);
+    }
+}
